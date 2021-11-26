e4895246a3271f9fd85124c725debf119531a94f
Malcolm Campbell <malcolm.campbell@canterbury.ac.nz>
2021-11-26 20:48:58 +1300

    Create install.R

---
+++ install.R
@@ -0,0 +1,9 @@
+install.packages("readr")
+install.packages("readxl")
+
+install.packages("dplyr")
+install.packages("lubridate")
+
+install.packages("sf")
+install.packages("tmap")
+install.packages("cartogram")
