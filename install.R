17c42c9e30e70f6f1520a76bb17037cc249f0f8d
Malcolm Campbell <malcolm.campbell@canterbury.ac.nz>
2021-12-02 22:05:55 +1300

    Update install.R

--- install.R
+++ install.R
@@ -1,12 +1,5 @@
 # set default repo
 options(repos = c(CRAN = "https://cloud.r-project.org"))
-
-# create local user library path (not present by default) https://stackoverflow.com/a/43283085
-dir.create(path = Sys.getenv("R_LIBS_USER"), showWarnings = FALSE, recursive = TRUE)
-
-# update everything and install to local user library path
-update.packages(ask = FALSE, instlib = Sys.getenv("R_LIBS_USER"))
-
 # install packages
 install.packages(c("tmaptools", "leaflet", "ggplot2", "sf", "tmap", "cartogram", "tidyverse", "lubridate", "terra"))
 #END
