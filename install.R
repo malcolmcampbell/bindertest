76ff39cd010e4a3652592744c299cfede8570a25
Malcolm Campbell <malcolm.campbell@canterbury.ac.nz>
2021-12-13 15:32:24 +1300

    Delete install.R

--- install.R
+++
@@ -1,5 +0,0 @@
-# set default repo
-options(repos = c(CRAN = "https://cloud.r-project.org"))
-# install packages
-install.packages(c("tmaptools", "leaflet", "ggplot2", "sf", "tmap", "cartogram", "tidyverse", "lubridate", "terra"))
-#END
